b4903f5659c052cbe1ebb6b430694c4380b8c9ef
Patrick Simmons <patrick.simmons@amd.com>
2026-03-18 18:44:57 -0500

    I think it works now

--- build_tools/generate_repro_checkout.py
+++ build_tools/generate_repro_checkout.py
@@ -4,13 +4,15 @@ import os
 import subprocess
 
 def get_output_for_cmd(cmd_str):
-    return subprocess.run(cmd_str,shell=True,stdout=subprocess.PIPE).stdout
+    return subprocess.run(cmd_str,shell=True,text=True,stdout=subprocess.PIPE).stdout
 
 def recurse_git_diff(diffcmd):
-    lines = map(get_output_for_cmd(diffcmd).readlines(),str.rstrip)
+    lines = list(map(str.rstrip,get_output_for_cmd(diffcmd).split('\n')))
     i=0
     while i<len(lines):
         diff_line = lines[i]
+        if diff_line=='':
+            break
         i+=1
         index_line = lines[i]
         if index_line.split(' ')[-1]=="160000":
@@ -18,11 +20,11 @@ def recurse_git_diff(diffcmd):
             submodule_line = lines[i]
             submodule = diff_line.split(' ')[-1][2:]
 
-            sha_dirty = submodule.split(' ')[-1]
+            sha_dirty = submodule_line.split(' ')[-1]
             sha_dirty_parts = sha_dirty.split('-')
             print("pushd "+submodule)
             print("git reset --hard "+sha_dirty_parts[0])
-            if len(sha_dirty_parts > 1) and sha_dirty_parts[1]=="dirty":
+            if len(sha_dirty_parts) > 1 and sha_dirty_parts[1]=="dirty":
                 recurse_git_diff(submodule)
             print("popd")
             i+=1
@@ -34,6 +36,6 @@ def recurse_git_diff(diffcmd):
                 i+=1
             print("DIRTY_EOF")
 
-print("git reset --hard "+get_output_for_cmd("git rev-parse HEAD~1").read().rstrip())
+print("git reset --hard "+get_output_for_cmd("git rev-parse HEAD~1").rstrip())
 recurse_git_diff("git diff")
 recurse_git_diff("git diff HEAD~1")
