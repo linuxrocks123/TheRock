44e18b963cb3cdb2b93afb558a3c9cc1968d67d8
Patrick Simmons <patrick.simmons@amd.com>
2026-03-18 17:38:27 -0500

    Initial attempt

---
+++ build_tools/generate_repro_checkout.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import os
+import subprocess
+
+def get_output_for_cmd(cmd_str):
+    return subprocess.run(cmd_str,shell=True,stdout=subprocess.PIPE).stdout
+
+def recurse_git_diff(subdir):
+    lines = map(get_output_for_cmd("git diff").readlines(),str.rstrip)
+    i=0
+    while i<len(lines):
+        diff_line = lines[i]
+        i+=1
+        index_line = lines[i]
+        if index_line.split(' ')[-1]=="160000":
+            i+=5
+            submodule_line = lines[i]
+            submodule = diff_line.split(' ')[-1][2:]
+
+            sha_dirty = submodule.split(' ')[-1]
+            sha_dirty_parts = sha_dirty.split('-')
+            print("pushd submodule")
+            print("git reset --hard "+sha_dirty_parts[0])
+            if len(sha_dirty_parts > 1) and sha_dirty_parts[1]=="dirty":
+                recurse_git_diff(submodule)
+            print("popd")
+            i+=1
+        else:
+            i+=1
+            print("patch -p1 <<DIRTY_EOF")
+            while i < len(lines) and lines[i].split(' ')[0]!="diff":
+                print(lines[i])
+                i+=1
+            print("DIRTY_EOF")
+
+print("git reset --hard "+get_output_for_cmd("git rev-parse HEAD").read().rstrip())
